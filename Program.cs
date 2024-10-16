c4792fa046f249ca48a5f3e1406f50e54c0d7296
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-16 09:08:38 +0200

    initial setup

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Oppgaver_til_18._10___Klasser__Interfaces_og_metoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
